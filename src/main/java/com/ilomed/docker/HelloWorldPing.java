edf2d21b55c810075bf8d8c74cd49796dccae5a3
ilya.valdman <ivald79@gmail.com>
2019-03-06 14:17:48 -0500

    HelloWorldPing change mesage

--- src/main/java/com/ilomed/docker/HelloWorldPing.java
+++ src/main/java/com/ilomed/docker/HelloWorldPing.java
@@ -4,7 +4,7 @@ public class HelloWorldPing {
 
     public static void main(String args[]) throws Exception{
         for(int i = 0 ; i < 100; i++){
-            System.out.println("ilyav1 Ping " + i );
+            System.out.println("ilyav, william, dino Ping " + i );
             Thread.sleep(1000);
         }
     }
